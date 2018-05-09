80f72a3fa54e2a354df504c3ff978624f2253d1f
Humam Behnam <humamb@kth.se>
2018-05-09 10:39:32 +0200

    Added options button

--- Game.java
+++ Game.java
@@ -32,15 +32,64 @@ import javafx.stage.Stage;
 public class Game extends Application {
 
 private Pane pane;
+private GameMenu menu;
+private int graphicsWindowX = 500; // Window size
+private int graphicsWindowY = 500; // Window size
+private int numGrid = 20;
+
     @Override
     public void start(Stage primaryStage) {
         gameMenu(primaryStage);
     }
 
     public void gameMenu(Stage primaryStage){
-        //size of window
-        int graphicsWindowX = 500;
-        int graphicsWindowY = 500;
+
+        //init graphic window
+        primaryStage.setTitle("Bomberman");
+
+        pane = new Pane();
+        pane.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+        primaryStage.setScene(new Scene(pane, graphicsWindowX ,graphicsWindowY));
+        Render render = new Render(pane, primaryStage);
+
+
+        ImageView bombermanLogo = new ImageView("BombermanInda/Images/BomberManTitle.png");
+        pane.getChildren().add(bombermanLogo);
+        bombermanLogo.setFitHeight(71);
+        bombermanLogo.setFitWidth(256);
+        bombermanLogo.relocate(125, 50);
+
+
+        // Makes a button that starts the game
+        Button startButton = new Button("Start");
+        pane.getChildren().add(startButton);
+        startButton.relocate(125, 300);
+        ImageView startButtonImage = new ImageView("BombermanInda/Images/MainCharFront.png");
+        startButton.setGraphic((Node) startButtonImage);
+        startButton.setOnMousePressed(e -> startGame(primaryStage));
+
+        // Makes a button that exits the program
+        Button quitButton = new Button("Quit");
+        pane.getChildren().add(quitButton);
+        quitButton.relocate(125, 100);
+        ImageView quitButtonImage = new ImageView("BombermanInda/Images/MainCharBack.png");
+        quitButton.setGraphic((Node) quitButtonImage);
+        quitButton.setOnMousePressed(e -> System.exit(1));
+
+        // Makes a button that takes takes you to the options
+        Button optionsButton = new Button("Options");
+        pane.getChildren().add(optionsButton);
+        optionsButton.relocate(125, 200);
+        ImageView optionsButtonImage = new ImageView("BombermanInda/Images/MainCharRight.png");
+        optionsButton.setGraphic((Node) optionsButtonImage);
+        optionsButton.setOnMousePressed(e -> optionsMenu(primaryStage));
+
+        primaryStage.show();
+
+
+    }
+
+    public void optionsMenu(Stage primaryStage) {
 
         //init graphic window
         primaryStage.setTitle("Bomberman");
@@ -49,34 +98,10 @@ private Pane pane;
         primaryStage.setScene(new Scene(pane, graphicsWindowX ,graphicsWindowY));
         Render render = new Render(pane, primaryStage);
 
-        //Makes a button that starts the game
-        Button startButton = new Button("Start");
-        pane.getChildren().add(startButton);
-        startButton.relocate(250, 300);
-        ImageView startButtonImage = new ImageView("BombermanInda/Images/MainCharFront.png");
-        startButton.setGraphic((Node) startButtonImage);
-        startButton.setOnMousePressed(e -> startGame(primaryStage));
-
-        //Makes a button that exits the program
-        Button quitButton = new Button("Quit");
-        pane.getChildren().add(quitButton);
-        quitButton.relocate(250, 100);
-        ImageView quitButtonImage = new ImageView("BombermanInda/Images/MainCharBack.png");
-        quitButton.setGraphic((Node) quitButtonImage);
-        quitButton.setOnMousePressed(e -> System.exit(1));
-
-        primaryStage.show();
-
-
-
 
     }
 
     public void startGame(Stage primaryStage) {
-
-        //size of window
-        int graphicsWindowX = 500;
-        int graphicsWindowY = 500;
 
         //init graphic window
         primaryStage.setTitle("Bomberman");
@@ -115,7 +140,9 @@ private Pane pane;
 
         // Gameloop
         final LongProperty lastUpdateTime = new SimpleLongProperty(0);
+
         new AnimationTimer(){
+
             @Override
             public void handle(long timestamp) {
                 long elapsedTimeMs = (timestamp - lastUpdateTime.get())/100000000000l;
@@ -123,12 +150,26 @@ private Pane pane;
                 world.moveAllMoveable(elapsedTimeMs);
 
 
-
             }
 
         }.start();
 
+
+        /**
+        //Exits from the game to the menu when ESC key is pressed
+        primaryStage.getScene().setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent ke) {
+                if (ke.getCode() == KeyCode .ESCAPE) {
+                gameMenu(primaryStage);
+                }
+            }
+        });
+        */
+
     }
+
+   
 }
     
 
