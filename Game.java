0a24463964386f6e3ea46a548585dee31ed400db
Humam Behnam <humamb@kth.se>
2018-05-20 14:01:19 +0200

    nothing important

--- Game.java
+++ Game.java
@@ -100,7 +100,7 @@ private int numWalls = (numGridX*numGridY)/2-1;
         helpButton.setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
-                pane.getChildren().removeAll();
+                pane.getChildren().remove(0, pane.getChildren().size());
                 helpMenu(primaryStage, pane);
             }
         });
@@ -137,7 +137,7 @@ private int numWalls = (numGridX*numGridY)/2-1;
         backButton.setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
-                pane.getChildren().removeAll();
+                pane.getChildren().remove(0, pane.getChildren().size());
                 gameMenu(primaryStage, pane);
             }
         });
@@ -273,6 +273,7 @@ private int numWalls = (numGridX*numGridY)/2-1;
         image.setFitHeight(graphicsWindowY/10);
         button.setGraphic(image);
         button.setPrefSize(graphicsWindowX/9, graphicsWindowY/9);
+        button.setStyle("-fx-background-color: #090a0c, linear-gradient(#38424b 0%, #1f2429 20%, #191d22 100%), linear-gradient(#20262b, #191d22), radial-gradient(center 50% 0%, radius 100%, rgba(114,131,148,0.9), rgba(255,255,255,0)); -fx-background-radius: 5,4,3,5; -fx-background-insets: 0,1,2,0; -fx-text-fill: white; -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 ); -fx-font-family; -fx-text-fill: linear-gradient(white, #d0d0d0); -fx-font-size: 12px; -fx-padding: 10 20 10 20;");
         button.relocate((graphicsWindowX/2)-button.getPrefWidth()/2, graphicsWindowY/yPos);
         return button;
     }
